ec18fea83c58b9fe46db8371809ba61265e44c09
JuanAddiel <addieljaquez@gmail.com>
2024-01-06 11:00:20 -0400

    Initial Creating

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+
+export const GifExpertApp =()=>{
+    return (
+      <>
+        {/*titulo*/}
+        <h1>GifExpertApp</h1>
+
+        {/*input*/}
+        {/*listado de gif*/}
+        {/*gif item*/}
+      </>
+    );
+}
